1768dca99c32e7755723f9efa69868c127f40f54
Sergey Kovalev <svkoval@mail.ru>
2025-07-13 18:01:24 +0900

    cicd

--- loader.py
+++ loader.py
@@ -1,13 +1,13 @@
 from telebot import TeleBot
 from telebot.storage import StateMemoryStorage
 from config_data.config import BOT_TOKEN
-import logging
+#import logging
 
-logger = logging.getLogger('EasyTravelBot')
-f_handler = logging.FileHandler('mybot.log')
-f_handler.setLevel(logging.ERROR)
-f_format = logging.Formatter(fmt='%(asctime)s,%(levelname)s,%(message)s', datefmt='%d.%m.%Y %H:%M:%S')
-f_handler.setFormatter(f_format)
-logger.addHandler(f_handler)
+#logger = logging.getLogger('EasyTravelBot')
+#f_handler = logging.FileHandler('mybot.log')
+#f_handler.setLevel(logging.ERROR)
+#f_format = logging.Formatter(fmt='%(asctime)s,%(levelname)s,%(message)s', datefmt='%d.%m.%Y %H:%M:%S')
+#f_handler.setFormatter(f_format)
+#logger.addHandler(f_handler)
 
 bot = TeleBot(BOT_TOKEN, state_storage=StateMemoryStorage())
