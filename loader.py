a5f4cb30252224202a90d4816c727ca0e0515316
Sergey Kovalev <svkoval@mail.ru>
2025-07-13 22:11:39 +0900

    cicd

--- loader.py
+++ loader.py
@@ -4,10 +4,4 @@ from config_data.config import BOT_TOKEN
 import logging
 
 logger = logging.getLogger('EasyTravelBot')
-#f_handler = logging.FileHandler('mybot.log')
-#f_handler.setLevel(logging.ERROR)
-#f_format = logging.Formatter(fmt='%(asctime)s,%(levelname)s,%(message)s', datefmt='%d.%m.%Y %H:%M:%S')
-#f_handler.setFormatter(f_format)
-#logger.addHandler(f_handler)
-
 bot = TeleBot(BOT_TOKEN, state_storage=StateMemoryStorage())
