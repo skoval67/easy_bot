2f4a6af369422d6568ee3d55f951dbd6ee43446b
Sergey Kovalev <svkoval@mail.ru>
2025-07-26 00:24:16 +0900

    cd

--- utils/rapidapi.py
+++ utils/rapidapi.py
@@ -42,6 +42,7 @@ def get_regions_id(location: str) -> List:
 
 def make_query(data: Dict) -> (bool, Dict):
   try:
+    debugpy.breakpoint()
     url = APIURL.format('properties/v2/list')
     headers = {
   	  "X-RapidAPI-Key": config.RAPID_API_KEY,
