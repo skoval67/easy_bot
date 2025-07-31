c38d36c367a209f4e443849a6ab459884d0baac7
Sergey Kovalev <svkoval@mail.ru>
2025-07-31 23:26:47 +0900

    cd

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import handlers
 from telebot.custom_filters import StateFilter
 from utils.set_bot_commands import set_default_commands
 from keyboards.inline.filters import bind_filters
-import debugpy
+#import debugpy
 
-debugpy.listen(("0.0.0.0", 5678))
-debugpy.wait_for_client()
+#debugpy.listen(("0.0.0.0", 5678))
+#debugpy.wait_for_client()
 
 if __name__ == '__main__':
   bot.add_custom_filter(StateFilter(bot))
